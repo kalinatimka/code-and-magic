a3e273e108abdb6dd1d70f8f44c2c18efafe19e5
Timofey <kalinata0799@gmail.com>
2019-02-08 18:21:57 +0300

    Начал вести историю

---
+++ js/setup.js
@@ -0,0 +1,117 @@
+var setupWindow = document.querySelector('.setup');
+var setupWindowOpen = document.querySelector('.setup-open');
+var setupWindowClose = document.querySelector('.setup-close');
+var setupWindowUsername = setupWindow.querySelector('.setup-user-name');
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+var setupSimilarList = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+var dataWizards = {
+  COUNT: 4,
+  NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+  SURNAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
+  COATS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+  EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green']
+};
+
+setupWindowOpen.addEventListener('click', function () {
+  openSetup();
+});
+
+setupWindowOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openSetup();
+  }
+});
+
+setupWindowUsername.addEventListener('focus', function () {
+  useEscExit(false);
+});
+
+setupWindowUsername.addEventListener('blur', function () {
+  useEscExit(true);
+});
+
+setupWindowClose.addEventListener('click', function () {
+  closeSetup();
+});
+
+setupWindowClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closeSetup();
+  }
+});
+
+// функция событие на закрытие окна по клавише ESC
+var onSetupWindowEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeSetup();
+  }
+}
+
+// функция открытия окна настройки персонажа
+var openSetup = function () {
+  setupWindow.classList.remove('hidden');
+  document.querySelector('.setup-similar').classList.remove('hidden');
+  useEscExit(true);
+};
+
+// функция закрытия окна настройки персонажа
+var closeSetup = function () {
+  setupWindow.classList.add('hidden');
+  useEscExit(false);
+};
+
+// функция добавления/удаления события для закрытия окна на ESC
+var useEscExit = function (flag) {
+  if (flag) {
+    document.addEventListener('keydown', onSetupWindowEscPress);
+  } else {
+    document.removeEventListener('keydown', onSetupWindowEscPress);
+  }
+};
+
+// функция рандомного объединения имени и фамилии волшебника
+var generateNames = function () {
+  return wizardFullName = dataWizards.NAMES[Math.floor(Math.random()*dataWizards.NAMES.length)] + ' ' + dataWizards.SURNAMES[Math.floor(Math.random()*dataWizards.SURNAMES.length)];
+};
+
+// функия нахождения рандомного элемента
+var generateRandomElement = function (mas) {
+  return mas[Math.floor(Math.random()*mas.length)];
+};
+
+// функия-генератор магов
+var generateWizards = function () {
+  var wizards = [];
+  for (var i = 0; i < dataWizards.COUNT; i++) {
+    wizards[i] = {
+      name: generateNames(),
+      coatColor: generateRandomElement(dataWizards.COATS),
+      eyesColor: generateRandomElement(dataWizards.EYES_COLORS)
+    };
+  }
+  return wizards;
+};
+
+// функция присвоения данных элементам разметки 
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
+// функия добавления схожих магов в разметку
+var renderWizards = function () {
+  var wizards = generateWizards();
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  setupSimilarList.appendChild(fragment);
+};
+
+renderWizards();
+console.dir(setupWindowOpen);
---
+++ js/stat.js
@@ -0,0 +1,52 @@
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 150;
+var CLOUD_Y = 10;
+var GAP = 10;
+var COLUMN_HEIGHT = 150;
+var COLUMN_WIDTH = 40;
+var COLUMN_GAP = 50;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+// var renderText = function() {};
+
+window.renderStatistics = function (ctx, names, times) {  
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
+
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = '#000000';
+  ctx.fillText('Ура, Вы победили!', CLOUD_X + GAP * 13, CLOUD_Y + GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP * 12, CLOUD_Y + (GAP * 3));
+  
+  var maxTime = getMaxElement(times);
+  console.log(maxTime);
+
+  for (var i = 0; i < names.length; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + (Math.random() * (1 - 0.2) + 0.2) + ')';
+    }
+    ctx.fillRect((CLOUD_X + (GAP * 5.5)) + ((COLUMN_WIDTH + COLUMN_GAP)* i), (CLOUD_Y + (GAP * 5)) + COLUMN_HEIGHT, COLUMN_WIDTH, -(Math.round((COLUMN_HEIGHT * times[i]) / maxTime)));
+    ctx.fillStyle = '#000000';
+    ctx.fillText(names[i], (CLOUD_X + (GAP * 5.5)) + ((COLUMN_WIDTH + COLUMN_GAP)* i), ((CLOUD_Y + (GAP * 5)) + COLUMN_HEIGHT) + GAP);
+    console.log(names[i], times[i]);
+  }
+};
